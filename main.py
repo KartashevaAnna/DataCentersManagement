86bd0ee848bb24348d5b92e13d031dba3fd58117
Anna Kartasheva <annakartashevamail@gmail.com>
2023-05-15 15:51:25 +0300

    gather functions to execute them in a strict order

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ async def print_hello() -> None:
     print("HELLO!")
 
 
+async def second_hello() -> None:
+    print("hi!")
+
+
 @app.on_event("startup")
 @repeat_every(seconds=60 * 60)
-def recurring_function() -> None:
-    asyncio.run(print_hello())
+# @repeat_every(seconds=3)
+async def recurring_function() -> None:
+    await asyncio.gather(print_hello(), second_hello())
+
+
+
