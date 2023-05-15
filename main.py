7613544113a73c3643b91a8b849ba17bf894f34e
Anna Kartasheva <annakartashevamail@gmail.com>
2023-05-15 16:05:31 +0300

    get api key from env

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 import asyncio
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENWEATHERAPIKEY = os.getenv("OPENWEATHERAPIKEY")
 
 app = FastAPI()
 
@@ -18,11 +24,16 @@ async def second_hello() -> None:
     print("hi!")
 
 
+async def api_key() -> None:
+    print(OPENWEATHERAPIKEY)
+
+
 @app.on_event("startup")
 @repeat_every(seconds=60 * 60)
 # @repeat_every(seconds=3)
 async def recurring_function() -> None:
-    await asyncio.gather(print_hello(), second_hello())
+    await asyncio.gather(print_hello(), second_hello(), api_key())
 
 
 
+print(OPENWEATHERAPIKEY)
