cf8e96c34e1e1f45b69b8e3424a9bce57491db1e
Anna Kartasheva <annakartashevamail@gmail.com>
2023-05-18 17:45:01 +0300

    create db in docker

--- app/weather_app/routers.py
+++ app/weather_app/routers.py
@@ -1,8 +1,23 @@
 from fastapi import APIRouter
 
+from app.sql_app.database import SessionLocal
+from app.sql_app.models import WeatherReport
+
 router = APIRouter()
+
+
+def get_db(*args, **kwargs):
+    return SessionLocal()
 
 
 @router.get("/ping")
 async def ping():
     return "pong!"
+
+
+@router.post("/weather")
+def show_weather():
+    db = get_db()
+    weather = db.query(WeatherReport).all()
+    db.close()
+    return weather
